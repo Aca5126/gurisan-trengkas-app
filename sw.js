da801c74352d1afc2b3c3a4b843b88be8a9e63f3
Aca5126 <aca_d_ace@yahoo.com>
2025-12-10 19:45:30 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,14 +7,14 @@ const urlsToCache = [
   './manifest.webmanifest'
 ];
 
-// Install service worker dan cache fail asas
+// Install: cache fail asas
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Aktifkan service worker
+// Activate: buang cache lama
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
@@ -25,7 +25,7 @@ self.addEventListener('activate', event => {
   );
 });
 
-// Fetch: cuba dari cache dahulu, fallback ke network
+// Fetch: cache-first fallback network
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response =>
