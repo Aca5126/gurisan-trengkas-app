0ffe9432095ae82b96ba530e4a8b290607114d34
Aca5126 <aca_d_ace@yahoo.com>
2025-12-17 11:47:32 +0800

    Delete src/serviceWorkerRegistration.js

--- src/serviceWorkerRegistration.js
+++
@@ -1,9 +0,0 @@
-export function register() {
-  if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-      navigator.serviceWorker
-        .register("/src/service-worker.js")
-        .catch((err) => console.error("SW registration failed:", err));
-    });
-  }
-}
