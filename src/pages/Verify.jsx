47460594e34e1c14d399f29abe6654bd02310837
Aca5126 <aca_d_ace@yahoo.com>
2025-12-22 17:56:36 +0800

    Update Verify.jsx

--- src/pages/Verify.jsx
+++ src/pages/Verify.jsx
@@ -5,8 +5,8 @@ import GuidePanel from "../components/GuidePanel";
 
 export default function Verify() {
   return (
-    <div className="p-6 grid grid-cols-3 gap-6">
-      <div className="col-span-2 space-y-6">
+    <div className="p-6 grid grid-cols-2 gap-6">
+      <div className="space-y-6">
         <Canvas />
         <ResultPanel />
         <GuidePanel />
