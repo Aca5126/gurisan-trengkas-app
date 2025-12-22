a0bed624d10bddee357e04d689037600f502e42c
Aca5126 <aca_d_ace@yahoo.com>
2025-12-22 17:42:37 +0800

    Update Verify.jsx

--- src/pages/Verify.jsx
+++ src/pages/Verify.jsx
@@ -1,6 +1,7 @@
 import Canvas from "../components/Canvas";
 import ResultPanel from "../components/ResultPanel";
 import HistoryPanel from "../components/HistoryPanel";
+import GuidePanel from "../components/GuidePanel";
 
 export default function Verify() {
   return (
@@ -8,10 +9,9 @@ export default function Verify() {
       <div className="col-span-2 space-y-6">
         <Canvas />
         <ResultPanel />
+        <GuidePanel />
       </div>
-      <div>
-        <HistoryPanel />
-      </div>
+      <HistoryPanel />
     </div>
   );
 }
